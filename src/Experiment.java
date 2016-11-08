fd35af0dc77c1ebb3129bc30f85cb6a461c75322
Fabián Souto <souto_94@hotmail.com>
2016-11-08 00:18:07 -0300

    Experiment reformatted.

--- src/Experiment.java
+++ src/Experiment.java
@@ -20,7 +20,7 @@ public class Experiment {
     private static int maxDim = 100;
 
     public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
-        for (int i = 10; i <= 25; i += 5) {
+        for (int i = 10; i <= 25; i += 1) {
             try {
                 experiment((int) Math.pow(2, i));
             } catch (GeneralException e) {
@@ -36,7 +36,7 @@ public class Experiment {
      * @param n the number of elements to be contained in each tree.
      */
     private static void experiment(int n) throws GeneralException, FileNotFoundException, UnsupportedEncodingException {
-        String nameFile = "Stats_n_" + n + (System.currentTimeMillis() / 1000) + ".txt";
+        String nameFile = "Stats_n_" + n + "_" + (System.currentTimeMillis() / 100000) + ".txt";
         PrintWriter writer = new PrintWriter(nameFile, "UTF-8");
 
         System.out.println("------------------");
@@ -53,14 +53,14 @@ public class Experiment {
         long startTime = System.currentTimeMillis();
         RTree linearTree = generateTree(randomDataset, new LinearSplit(m, M));
         long stopTime = System.currentTimeMillis();
-        System.out.println("Linear split creation time: " + (stopTime - startTime) + " ms.");
-        writer.println("Linear split creation time: " + (stopTime - startTime) + " ms.");
+        System.out.println("Linear split creation time: " + format((stopTime - startTime)) + " ms.");
+        writer.println("Linear split creation time: " + format(stopTime - startTime) + " ms.");
 
         startTime = System.currentTimeMillis();
         RTree greeneTree = generateTree(randomDataset, new GreeneSplit(m, M));
         stopTime = System.currentTimeMillis();
-        System.out.println("Greene split creation time: " + (stopTime - startTime) + " ms.");
-        writer.println("Greene split creation time: " + (stopTime - startTime) + " ms.");
+        System.out.println("Greene split creation time: " + format(stopTime - startTime) + " ms.");
+        writer.println("Greene split creation time: " + format(stopTime - startTime) + " ms.");
 
         // Measure usage percentage
         System.out.println("Linear split usage percentage: " + linearTree.usagePercentage());
@@ -70,33 +70,76 @@ public class Experiment {
         writer.println("Greene split usage percentage: " + greeneTree.usagePercentage());
 
         // Measure search performance in time units
+        long batchSize = 50;
+        System.out.println("QUERIES: batch size " + batchSize);
         startTime = System.currentTimeMillis();
-        for (Data data : queries) {
-            linearTree.search(data);
+        long lastTime = startTime;
+        for (int i = 0; i < queries.length; i++) {
+            if (i % batchSize == 0 && i!=0) {
+                StringBuilder toShow = new StringBuilder(" - Queries until now:").append(i).append("\n");
+                toShow.append(" - Time elapsed from beginning:").append(format(System.currentTimeMillis() - startTime)).append("\n");
+                toShow.append(" - Time last batch:").append(format(System.currentTimeMillis() - lastTime)).append("\n").append("\n");
+                lastTime = System.currentTimeMillis();
+                System.out.println(toShow.toString());
+                writer.println(toShow.toString());
+            }
+            linearTree.search(queries[i]);
         }
         stopTime = System.currentTimeMillis();
-        System.out.println("Linear split queries time: " + (stopTime - startTime) + " ms");
-        writer.println("Linear split queries time: " + (stopTime - startTime) + " ms");
+        System.out.println("Linear split queries time: " + format(stopTime - startTime) + " ms");
+        writer.println("Linear split queries time: " + format(stopTime - startTime) + " ms");
 
         startTime = System.currentTimeMillis();
-        for (Data data : queries) {
-            linearTree.search(data);
+        lastTime = startTime;
+        for (int i = 0; i < queries.length; i++) {
+            if (i % batchSize == 0 && i!=0) {
+                StringBuilder toShow = new StringBuilder(" - Queries until now:").append(i).append("\n");
+                toShow.append(" - Time elapsed from beginning:").append(format(System.currentTimeMillis() - startTime)).append("\n");
+                toShow.append(" - Time last batch:").append(format(System.currentTimeMillis() - lastTime)).append("\n");
+                lastTime = System.currentTimeMillis();
+                System.out.println(toShow.toString());
+                writer.println(toShow.toString());
+            }
+            greeneTree.search(queries[i]);
         }
         stopTime = System.currentTimeMillis();
-        System.out.println("Greene split queries time: " + (stopTime - startTime) + " ms");
-        writer.println("Greene split queries time: " + (stopTime - startTime) + " ms");
+        System.out.println("Greene split queries time: " + format(stopTime - startTime) + " ms");
+        writer.println("Greene split queries time: " + format(stopTime - startTime) + " ms");
 
         // Measure search performance in disc accesses
-        int discAccesses = 0;
-        for (Data data : queries) {
-            discAccesses += linearTree.accessCountSearch(data);
+        long discAccesses = 0;
+        System.out.println("DISC ACCESS: Batch size " + batchSize);
+        lastTime = System.currentTimeMillis();
+        startTime = lastTime;
+        for (int i = 0; i < queries.length; i++) {
+            if (i % batchSize == 0 && i!=0) {
+                StringBuilder toShow = new StringBuilder(" - Queries until now:").append(i).append("\n");
+                toShow.append(" - Disc access until now:").append(discAccesses).append("\n");
+                toShow.append(" - Time from beginning:").append(format(System.currentTimeMillis() - startTime)).append("\n");
+                toShow.append(" - Time last batch:").append(format(System.currentTimeMillis() - lastTime)).append("\n");
+                lastTime = System.currentTimeMillis();
+                System.out.println(toShow.toString());
+                writer.println(toShow.toString());
+            }
+            discAccesses += linearTree.accessCountSearch(queries[i]);
         }
         System.out.println("Linear split number of accesses: " + discAccesses);
         writer.println("Linear split number of accesses: " + discAccesses);
 
         discAccesses = 0;
-        for (Data data : queries) {
-            discAccesses += linearTree.accessCountSearch(data);
+        lastTime = System.currentTimeMillis();
+        startTime = lastTime;
+        for (int i = 0; i < queries.length; i++) {
+            if (i % batchSize == 0 && i!=0) {
+                StringBuilder toShow = new StringBuilder(" - Queries until now:").append(i).append("\n");
+                toShow.append(" - Disc access until now:").append(discAccesses).append("\n");
+                toShow.append(" - Time from beginning:").append(format(System.currentTimeMillis() - startTime)).append("\n");
+                toShow.append(" - Time last batch:").append(format(System.currentTimeMillis() - lastTime)).append("\n");
+                lastTime = System.currentTimeMillis();
+                System.out.println(toShow.toString());
+                writer.println(toShow.toString());
+            }
+            discAccesses += greeneTree.accessCountSearch(queries[i]);
         }
         System.out.println("Greene split number of accesses: " + discAccesses);
         writer.println("Greene split number of accesses: " + discAccesses);
@@ -112,6 +155,7 @@ public class Experiment {
      * @return the root of the resulting tree.
      * @throws GeneralException raised when a Data with no dimensions is inserted.
      */
+
     private static RTree generateTree(Data[] data, Splitter splitter) throws GeneralException {
         RTree tree = new RTree(m, M, splitter);
         for (Data d : data) {
@@ -143,5 +187,19 @@ public class Experiment {
             }
         }
         return data;
+    }
+
+    /**
+     * Formats a given time in milliseconds to HH:MM:SS:MS.
+     *
+     * @param time in milliseconds.
+     * @return time in format HH:MM:SS:MS.
+     */
+    private static String format(long time) {
+        long ms = time % 1000;
+        long seg = (time / 1000) % 60;
+        long min = (time / (1000 * 60)) % 60;
+        long hour = (time / (1000 * 60 * 60)) % 60;
+        return hour + ":" + min + ":" + seg + ":" + ms;
     }
 }
